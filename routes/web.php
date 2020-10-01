4f6f3c442385176ef06f0d4db5c612dfefb212e5
Jeremiah <50832578+JeremiahKoeiman@users.noreply.github.com>
2020-10-01 12:15:41 +0200

    Layout created

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.layout');
 });
+
+Route::resource('/books', 'BookController');
+Route::resource('/categories', 'CategoryController');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
